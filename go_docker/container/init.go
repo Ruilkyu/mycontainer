c0a28f5415cbca99b4f420a325c17a16f5cf2e38
lurui <lurui@ipfsforce.com>
2020-05-07 19:37:50 +0800

    pivot root

--- go_docker/container/init.go
+++ go_docker/container/init.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -26,6 +27,48 @@ import (
 //}
 
 
+
+
+
+// init挂载点
+func pivotRoot(root string) error{
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND | syscall.MS_REC, ""); err != nil {
+		return fmt.Errorf("Mount rootfs to itself error:%v", err)
+	}
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		return err
+	}
+	if err := syscall.PivotRoot(root, pivotDir); err != nil{
+		return fmt.Errorf("pivot_root %v", err)
+	}
+
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err!=nil{
+		return fmt.Errorf("unmount pivot_root dir %v", err)
+	}
+
+	return os.Remove(pivotDir)
+}
+
+
+func setUpMount(){
+	pwd, err := os.Getwd()
+	if err != nil{
+		log.Errorf("Get current location error %v", err)
+	}
+	log.Infof("Current location is %s", pwd)
+	pivotRoot(pwd)
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID | syscall.MS_STRICTATIME, "mode=755")
+}
+
+
 func readUserCommand() []string{
 	pipe := os.NewFile(uintptr(3), "pipe")
 	msg, err := ioutil.ReadAll(pipe)
@@ -43,8 +86,8 @@ func RunContainerInitProcess() error{
 		return fmt.Errorf("run container get user command error, cmdArray is nil")
 	}
 
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	setUpMount()
+
 	path, err := exec.LookPath(cmdArray[0])
 	if err != nil{
 		log.Errorf("exec loop path error %v", err)
